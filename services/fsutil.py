723c75df36518f540d5ad91683fb5e6d62c3a5f7
wanghaifeng <spafka@iclud.com>
2024-10-13 23:34:11 +0800

    lock

--- services/fsutil.py
+++ services/fsutil.py
@@ -6,7 +6,7 @@ import shutil
 from contextlib import contextmanager
 import time
 import psycopg2
-from typing import Callable, TypeVar
+from typing import Callable, TypeVar, Optional
 
 
 def get_db_connection():
@@ -18,37 +18,51 @@ T = TypeVar('T')
 V = TypeVar('V')
 
 
-def acquire_lock(func, lock_name: str, timeout: int = 10):
-    conn = get_db_connection()
-    start_time = time.time()
-    while True:
-        try:
-            conn.execute("INSERT INTO distributed_locks (lock_name,cts) VALUES (:lock,:time)",
-                         {"lock": lock_name, "time": datetime.datetime.now()})
-            conn.commit()
-            try:
-                print("get lock!!!", "execute func")
-                func()
-            except Exception as e:
-                print(e)
-            finally:
-                release_lock(lock_name)
-            return True
-        except Exception as e:  # 锁已被其他会话持有
-            conn.rollback()
-            if time.time() - start_time > timeout:
-                r = conn.execute("SELECT * FROM distributed_locks WHERE lock_name = :lock",
-                                 {"lock": lock_name}).fetchall()
-                if (len(r) > 0):
-                    if time.time() - time.mktime(r[0][1].timetuple()) > timeout:
-                        release_lock(lock_name)
-                        return
+def lock(lock_name: Optional[str] = None, timeout: int = 10):
+    def outer(func):
+        print(lock_name)
+
+        def inner(*agrs, **kwargs):
+            conn = get_db_connection()
+            start_time = time.time()
+            lock_key = lock_name;
+            if lock_key is None:
+                if "lock_name" in kwargs:
+                    lock_key = kwargs["lock_name"]
                 else:
-                    raise TimeoutError(f"Could not acquire lock {lock_name} within {timeout} seconds.")
-        time.sleep(1)
+                    raise KeyError("lock_name not set")
+            while True:
+                try:
+                    conn.execute("INSERT INTO distributed_locks (lock_name,cts) VALUES (:lock,:time)",
+                                 {"lock": lock_key, "time": datetime.datetime.now()})
+                    conn.commit()
+                    try:
+                        print("get lock!!!", "execute func")
+                        return func(*agrs, **kwargs)
+                    except Exception as e:
+                        print(e)
+                    finally:
+                        _release_lock(lock_key)
+                    return True
+                except Exception as e:  # 锁已被其他会话持有
+                    conn.rollback()
+                    if time.time() - start_time > timeout:
+                        r = conn.execute("SELECT * FROM distributed_locks WHERE lock_name = :lock",
+                                         {"lock": lock_key}).fetchall()
+                        if len(r) > 0:
+                            if time.time() - time.mktime(r[0][1].timetuple()) > timeout:
+                                _release_lock(lock_name)
+                                return
+                        else:
+                            raise TimeoutError(f"Could not acquire lock {lock_name} within {timeout} seconds.")
+                time.sleep(1)
+
+        return inner
+
+    return outer
 
 
-def release_lock(lock_name):
+def _release_lock(lock_name):
     conn = get_db_connection()
     conn.execute("DELETE FROM distributed_locks WHERE lock_name = :lock", {"lock": lock_name})
     conn.commit()
@@ -73,6 +87,7 @@ def print_tree(startpath):
             print(f'{subindent}{f}')
 
 
+@lock(timeout=10)
 def find_files(directory, extension):
     found_files = []
     for root, dirs, files in os.walk(directory):
@@ -111,46 +126,48 @@ from models import Result
 if __name__ == '__main__':
     # walk("./../")
     # print_tree("./../")
-    pys = find_files("./../", "services")
+    # pys = find_files("./../", "services")
+    #
+    # for py in pys:
+    #     print(py)
+    #
+    # list1 = [Result(url="www.baidu.com", id=1, result_all=None, result_no_stop_words=None),
+    #          Result(url="www.google.com", id=2, result_all=None, result_no_stop_words=None),
+    #          Result(url="www.sohu.com", id=3, result_all=None, result_no_stop_words=None)]
+    # list2 = [Result(url="www.baidu.com", id=2, result_all=None, result_no_stop_words=None),
+    #          Result(url="www.taobao.com", id=1, result_all=None, result_no_stop_words=None),
+    #          Result(url="www.sohu.com", id=4, result_all=None, result_no_stop_words=None)]
+    #
+    # # 使用 & 操作符
+    # intersection_set = set(list1) & set(list2)
+    # print("交集:", intersection_set)
+    #
+    # # 或者使用 intersection() 方法
+    # intersection_set = set(list1).intersection(set(list2))
+    # print("交集:", intersection_set)
+    #
+    # # 使用 | 操作符
+    # union_set = set(list1) | set(list2)
+    # print("并集:", union_set)
+    #
+    # # 或者使用 union() 方法
+    # union_set = set(list1).union(set(list2))
+    # print("并集:", union_set)
+    #
+    # # 使用 - 操作符
+    # difference_set_1 = set(list1) - set(list2)
+    # print("list1 中存在但 list2 中不存在的元素:", difference_set_1)
+    #
+    # # 或者使用 difference() 方法
+    # difference_set_1 = set(list1).difference(set(list2))
+    # print("list1 中存在但 list2 中不存在的元素:", difference_set_1)
+    #
+    # # 使用 - 操作符
+    # difference_set_2 = set(list2) - set(list1)
+    # print("list2 中存在但 list1 中不存在的元素:", difference_set_2)
+    #
+    # # 或者使用 difference() 方法
+    # difference_set_2 = set(list2).difference(set(list1))
+    # print("list2 中存在但 list1 中不存在的元素:", difference_set_2)
 
-    for py in pys:
-        print(py)
-
-    list1 = [Result(url="www.baidu.com", id=1, result_all=None, result_no_stop_words=None),
-             Result(url="www.google.com", id=2, result_all=None, result_no_stop_words=None),
-             Result(url="www.sohu.com", id=3, result_all=None, result_no_stop_words=None)]
-    list2 = [Result(url="www.baidu.com", id=2, result_all=None, result_no_stop_words=None),
-             Result(url="www.taobao.com", id=1, result_all=None, result_no_stop_words=None),
-             Result(url="www.sohu.com", id=4, result_all=None, result_no_stop_words=None)]
-
-    # 使用 & 操作符
-    intersection_set = set(list1) & set(list2)
-    print("交集:", intersection_set)
-
-    # 或者使用 intersection() 方法
-    intersection_set = set(list1).intersection(set(list2))
-    print("交集:", intersection_set)
-
-    # 使用 | 操作符
-    union_set = set(list1) | set(list2)
-    print("并集:", union_set)
-
-    # 或者使用 union() 方法
-    union_set = set(list1).union(set(list2))
-    print("并集:", union_set)
-
-    # 使用 - 操作符
-    difference_set_1 = set(list1) - set(list2)
-    print("list1 中存在但 list2 中不存在的元素:", difference_set_1)
-
-    # 或者使用 difference() 方法
-    difference_set_1 = set(list1).difference(set(list2))
-    print("list1 中存在但 list2 中不存在的元素:", difference_set_1)
-
-    # 使用 - 操作符
-    difference_set_2 = set(list2) - set(list1)
-    print("list2 中存在但 list1 中不存在的元素:", difference_set_2)
-
-    # 或者使用 difference() 方法
-    difference_set_2 = set(list2).difference(set(list1))
-    print("list2 中存在但 list1 中不存在的元素:", difference_set_2)
+    print(find_files("./", ".py",lock_name="xxx"))
